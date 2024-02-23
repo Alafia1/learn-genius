a48ea5d6d3e3632527f59abcd48fc21bfef1dead
Alafia1 <mdmuhab@gmail.com>
2024-02-23 15:09:11 +0100

    added forward ref to each chapters card

--- src/components/ConfirmChapters.tsx
+++ src/components/ConfirmChapters.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import { Chapter, Course, Unit } from "@prisma/client";
 import React from "react";
+import ChapterCard, { ChapterCardHandler } from "./ChapterCard";
+import { Separator } from "./ui/separator";
+import Link from "next/link";
+import { Button, buttonVariants } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 type Props = {
   course: Course & {
@@ -10,7 +17,64 @@ type Props = {
 };
 
 const ConfirmChapters = ({ course }: Props) => {
-  return <div>ConfirmChapters</div>;
+  const chapterRefs: Record<string, React.RefObject<ChapterCardHandler>> = {};
+  course.units.forEach((unit) => {
+    unit.chapters.forEach((chapter) => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      chapterRefs[chapter.id] = React.useRef(null);
+    });
+  });
+  return (
+    <div className="w-full mt-4">
+      {course.units.map((unit, unitIndex) => {
+        return (
+          <div key={unit.id} className="mt-5">
+            <h2 className="text-sm uppercase text-secondary-foreground/60">
+              Unit {unitIndex + 1}
+            </h2>
+            <h3 className="tesxt-2xl font-bold">{unit.name}</h3>
+            <div className="mt-3">
+              {unit.chapters.map((chapter, index) => {
+                return (
+                  <ChapterCard
+                    ref={chapterRefs[chapter.id]}
+                    key={chapter.id}
+                    chapter={chapter}
+                    index={index}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <div className="flex items-center justify-center mt-4">
+        <Separator className="flex-[1]" />
+        <div className="flex items-center mx-4">
+          <Link
+            href="/create"
+            className={buttonVariants({ variant: "secondary" })}
+          >
+            <ChevronLeft className="w-4 h-4 ml-2" strokeWidth={4} />
+            Back
+          </Link>
+          <Button
+            type="button"
+            className="ml-4 font-semibold"
+            onClick={() => {
+              Object.values(chapterRefs).forEach((ref) => {
+                ref.current?.triggerLoad();
+              });
+            }}
+          >
+            Generate
+            <ChevronRight className="w-4 h-4 ml-2" strokeWidth={4} />
+          </Button>
+        </div>
+        <Separator className="flex-[1]" />
+      </div>
+    </div>
+  );
 };
 
 export default ConfirmChapters;
