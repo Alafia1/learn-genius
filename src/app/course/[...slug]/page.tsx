fae8340eefa0004b805cf7357ff4c3d7f8b5c717
Alafia1 <mdmuhab@gmail.com>
2024-02-26 16:03:43 +0100

    created the video component

---
+++ src/app/course/[...slug]/page.tsx
@@ -0,0 +1,60 @@
+import CourseSideBar from "@/components/CourseSideBar";
+import MainVideoSummary from "@/components/MainVideoSummary";
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    slug: string[];
+  };
+};
+
+const CoursePage = async ({ params: { slug } }: Props) => {
+  const [courseId, unitIndexParam, chapterIndexParams] = slug;
+  const course = await prisma.course.findUnique({
+    where: { id: courseId },
+    include: {
+      units: {
+        include: {
+          chapters: true,
+        },
+      },
+    },
+  });
+  if (!course) {
+    return redirect("/gallery");
+  }
+  let unitIndex = parseInt(unitIndexParam);
+  let chapterIndex = parseInt(chapterIndexParams);
+
+  const unit = course.units[unitIndex];
+  if (!unit) {
+    return redirect("/gallery");
+  }
+
+  const chapter = unit.chapters[chapterIndex];
+  if (!chapter) {
+    return redirect("/gallery");
+  }
+  return (
+    <div>
+      <CourseSideBar course={course} currentChapterId={chapter.id} />
+      <div>
+        <div className="ml-[400px] px-8">
+          <div className="flex">
+            <MainVideoSummary
+              chapter={chapter}
+              unit={unit}
+              chapterIndex={chapterIndex}
+              unitIndex={unitIndex}
+            />
+            {/* <QuizCard /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoursePage;
